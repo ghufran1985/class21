67a93d08def14cdbef953b033763a630d21c1db9
ghufran1985 <147316168+ghufran1985@users.noreply.github.com>
2025-03-06 20:27:56 +0300

    Add files via upload

---
+++ class21/o1time.py
@@ -0,0 +1,10 @@
+def printno(n):
+    iteratition = 0
+    print('the number enterd by the user is:', n)
+    iteratition = iteratition + 1
+    print('total iteration by the code is:', iteratition)
+
+printno(5)
+printno(10)
+
+print('with any n the time taken by our code would not change')
